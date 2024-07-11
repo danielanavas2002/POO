063f52bae4ce673cf016f2561ed6182f9b235f75
danielanavas2002 <nav211000@uvg.edu.gt>
2024-07-10 22:07:52 -0600

    Cambios en Main y Serie

--- Ejercicios/01Clases/Main.java
+++ Ejercicios/01Clases/Main.java
@@ -2,11 +2,24 @@ class Main
 {
     public static void main(String[] args) 
     {
-        Serie lucifer = new Serie("Lucifer", 93, 8.1f, "Netflix", "Watched");
-        System.out.println(lucifer.getName());
-        System.out.println(lucifer.getEpisodes());
-        System.out.println(lucifer.getIMDb());
-        System.out.println(lucifer.getPlatform());
-        System.out.println(lucifer.getBookMark());
+        Serie lucifer = new Serie("Lucifer", 93, 8.1f, "Netflix", "Watched", 93);
+        // System.out.println(lucifer.getName());
+        // System.out.println(lucifer.getEpisodes());
+        // System.out.println(lucifer.getIMDb());
+        // System.out.println(lucifer.getPlatform());
+        // System.out.println(lucifer.getStatus());
+        System.out.println(lucifer.getInformation());
+        System.out.println(lucifer.getName() + "'s Status: " + lucifer.getStatus());
+        System.out.println(lucifer.getWatched());
+
+        Serie greys = new Serie("Grey's Anatomy", 428, 7.6f, "Disney+", "Watching", 420);
+        System.out.println(greys.getInformation());
+        System.out.println(greys.getName() + "'s Status: " + greys.getStatus());
+        System.out.println(greys.getWatched());
+
+        Serie thisisus = new Serie("This Is Us", 106, 8.7f, "Amazon Prime Video", "Plan to Watch", 0);
+        System.out.println(thisisus.getInformation());
+        System.out.println(thisisus.getName() + "'s Status: " + thisisus.getStatus());
+        System.out.println(thisisus.getWatched());
     }
 }
--- Ejercicios/01Clases/Serie.java
+++ Ejercicios/01Clases/Serie.java
@@ -4,16 +4,41 @@ class Serie
     private int episodes;
     private float imdb;
     private String platform;
-    private String bookmark;
+    private String status;
+    private int watched;
 
-    public Serie(String name, int episodes, float imdb, String platform, String bookmark)
+    public Serie(String name, int episodes, float imdb, String platform, String status, int watched)
     {
         this.name = name;
         this.episodes = episodes;
         this.imdb = imdb;
         this.platform = platform;
-        this.bookmark = bookmark;
+        this.status = status;
+        this.watched = watched;
     } 
+
+    public String getInformation()
+    {
+        String text = " ";
+        text = this.name + " has " + this.episodes + " episodes, with a rating of " + this.imdb + " on IMDb and is available on " + this.platform + "."; 
+        return text;
+    }
+
+    public String getWatched()
+    {
+        String text = "";
+        int pending = this.episodes - this.watched;
+        if (pending > 0){
+            text = "Pending Episodes: " + pending;
+        } 
+        else if (pending == 0 ){
+            text = "No Pending Episodes";
+        } 
+        else
+            text = "Error"; 
+
+        return text;
+    }
 
     public String getName() 
     {
@@ -35,8 +60,8 @@ class Serie
         return platform;
     }
 
-    public String getBookMark() 
+    public String getStatus() 
     {
-        return bookmark;
+        return status;
     }
 }
