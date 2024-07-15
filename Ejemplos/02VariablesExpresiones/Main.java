023c65e0d60e9394453f242439addadb41ad7bb6
danielanavas2002 <nav211000@uvg.edu.gt>
2024-07-15 14:50:50 -0600

    Ejemplo02

---
+++ Ejemplos/02VariablesExpresiones/Main.java
@@ -0,0 +1,16 @@
+class Main
+{
+    public static void main(String[] args){
+        int messi = 10;
+
+
+        System.out.println(messi);
+
+        // messi = "Campeón";
+        // System.out.println(messi);
+
+        final float pi = 3.1415f;
+        // pi = 3.16f;
+        System.out.println(pi);
+    }
+}
